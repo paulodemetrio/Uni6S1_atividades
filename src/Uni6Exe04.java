12e573635165f5d88d77381622b10db2833246d7
paulodemetrio <pdemetriocom@gmail.com>
2025-05-07 19:57:51 -0300

    continuacao

--- src/Uni6Exe04.java
+++ src/Uni6Exe04.java
@@ -4,37 +4,50 @@ import java.util.Scanner;
 
 public class Uni6Exe04 {
     public static void main(String[] args) {
-        int[] vetor1 = Ler();
-        int[] vetor2 = Ler();
-        int vetor3[] = null;
-        vetor3 = Soma(vetor1, vetor2, vetor3);
-        Escrever(vetor3);
-    }
-
-    private static int[] Ler(){
         Scanner scan = new Scanner(System.in);
+        int[] vetor1 = Ler(scan);
+        int[] vetor2 = Ler(scan);
+        int vetor3[] = new int[10];
+        vetor3 = Soma(vetor1, vetor2, vetor3);
+        Escrever(vetor1, vetor2, vetor3);
+        scan.close();
+    }
+    
+    private static int[] Ler(Scanner scan) {
         int vetor[] = new int[10];
-        System.out.println("Digite os valores: ");
+        System.out.print("Digite os valores: ");
         for (int i = 0; i < vetor.length; i++) {
             vetor[i] = scan.nextInt();
         }
-        scan.close();
         return vetor;
     }
-
-    private static int[] Soma(int[] vetor1, int[] vetor2, int[] vetor3){
-        for (int i = 0; i < vetor1.length; i++) {
-            for (int j = 0; j < vetor2.length; j++) {
-                vetor3[i] = vetor1[i] + vetor2[i];
-            }
+    
+    private static int[] Soma(int[] vetor1, int[] vetor2, int[] vetor3) {
+        for (int i = 0; i < vetor3.length; i++) {
+            vetor3[i] = vetor1[i] + vetor2[i];
         }
         return vetor3;
     }
-
-    private static void Escrever(int[] vetor) {
-        System.out.println("Os valores somados resultaram no seguinte vetor:");
-        for (int i = vetor.length - 1; i >= 0; i--) {
-            System.out.print(vetor[i] + " | ");
+    
+    private static void Escrever(int[] vetor1, int[] vetor2, int[] vetor3) {
+        for (int i = 1; i < 4; i++) {
+            if (i == 1) {
+                System.out.println("Valores do vetor " + i + " : ");
+                for (int j = 0; j < vetor1.length; j++) {
+                    System.out.print(vetor1[j] + " | ");
+                }
+            } else if (i == 2) {
+                System.out.println("Valores do vetor " + i + " : ");
+                for (int j = 0; j < vetor2.length; j++) {
+                    System.out.print(vetor2[j] + " | ");
+                }
+            } else if (i == 3) {
+                System.out.println("Valores do vetor " + i + " (vetor " + (i - 2) + " + vetor " + (i - 1) + ") : ");
+                for (int j = 0; j < vetor3.length; j++) {
+                    System.out.print(vetor3[j] + " | ");
+                }
+            }
+            System.out.println();
         }
     }
 }
